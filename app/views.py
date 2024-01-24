e627e7c525017efea4b74c18680ec4e9a3eab44c
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:36:51 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # yourappname/views.py
 from rest_framework import viewsets
-from .models import Contact, Mentorship,Project_Planner
-from .serializers import ContactSerializer, MentorSerializer,ProjectSerializer
+from .models import Contact, Mentorship,Project_Planner,Projects,DisplayImage, Testmonials
+from .serializers import ContactSerializer, MentorSerializer,ProjectSerializer,ProjectsSerializer, DisplayImageSerializer, TestmonialsSerializer
 
 class ContactViewSet(viewsets.ModelViewSet):
     authentication_classes = []
@@ -20,4 +20,17 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project_Planner.objects.all()
     serializer_class = ProjectSerializer
 
+class ProjectsViewSet(viewsets.ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+
+
+class DisplayImageViewSet(viewsets.ModelViewSet):
+    queryset = DisplayImage.objects.all()
+    serializer_class = DisplayImageSerializer
+
+
+class TestmonialsViewSet(viewsets.ModelViewSet):
+    queryset = Testmonials.objects.all()
+    serializer_class = TestmonialsSerializer
 
