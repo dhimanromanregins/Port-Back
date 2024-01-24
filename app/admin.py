3d1f29ccd3b0b7592ba83472ed45634c463b6943
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:35:47 +0530

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Contact, Project_Planner, Mentorship
+from .models import Contact, Mentorship,Project_Planner,Projects,DisplayImage, Testmonials
 # Register your models here.
 
-admin.site.register((Contact, Project_Planner, Mentorship))
+admin.site.register(Contact)
+admin.site.register(Mentorship)
+admin.site.register(Project_Planner)
+admin.site.register(Projects)
+admin.site.register(DisplayImage)
+admin.site.register(Testmonials)
