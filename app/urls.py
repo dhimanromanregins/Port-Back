8dc9da98e0e63a41925d44d465ba69813e89ca6d
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:36:37 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContactViewSet, MentorViewSet,  ProjectViewSet
+from .views import ContactViewSet, MentorViewSet,  ProjectViewSet, ProjectsViewSet,DisplayImageViewSet, TestmonialsViewSet
 
 router = DefaultRouter()
 router.register(r'contacts', ContactViewSet, basename='contact')
 router.register(r'mentor', MentorViewSet, basename='mentor')
 router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'project-display', ProjectsViewSet, basename='Project-display')
+router.register(r'testmonials', TestmonialsViewSet, basename='testmonials')
 
 urlpatterns = [
     path('', include(router.urls)),
