c7478ed8e503f05e7dbef37f3c7164d9165d67a6
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:36:22 +0530

    Update serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Contact, Mentorship,Project_Planner
+from .models import Contact, Mentorship,Project_Planner,Projects,DisplayImage, Testmonials
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +16,21 @@ class MentorSerializer(serializers.ModelSerializer):
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project_Planner
+        fields = '__all__'
+
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
+
+
+class DisplayImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DisplayImage
+        fields = '__all__'
+
+class TestmonialsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testmonials
         fields = '__all__'
