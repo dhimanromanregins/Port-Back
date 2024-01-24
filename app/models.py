bed137498d8d2cac67e1c8bbca6239b6dc6f1836
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:36:10 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -24,3 +24,19 @@ class Project_Planner(models.Model):
     project_type = models.CharField(max_length=250)
     budget = models.CharField(max_length=250)
     description = models.TextField(max_length=1000)
+
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=250)
+    project_description = models.TextField(max_length=1000)
+    project_category = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='projects/')
+
+class DisplayImage(models.Model):
+    image = models.ImageField(upload_to='display-images/')
+
+class Testmonials(models.Model):
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='testimonials/')
+    description = models.TextField(max_length=1000)
+    Mentored_for = models.CharField(max_length=250)
