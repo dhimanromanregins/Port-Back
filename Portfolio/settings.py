0f80f2e35cfbd73f1fd9445a49283911628ac64d
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2024-01-24 16:35:20 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = 'back-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
